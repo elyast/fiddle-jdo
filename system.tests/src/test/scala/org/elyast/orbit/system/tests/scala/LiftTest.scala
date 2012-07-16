2270b966ad764eb6d6b0ab978293529213cff724
Lukasz Jastrzebski <lukasz.jastrzebski@gmail.com>
2012-07-16 13:19:33 -0700

    added tests for lift and unfiltered

---
+++ system.tests/src/test/scala/org/elyast/orbit/system/tests/scala/LiftTest.scala
@@ -0,0 +1,16 @@
+package org.elyast.orbit.system.tests.scala
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.WordSpec
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class LiftTest extends WordSpec with ShouldMatchers {
+
+  "Lift" should {
+    "start as a web application" in {
+      
+    }
+  }
+}
---
+++ system.tests/src/test/scala/org/elyast/orbit/system/tests/scala/UnfilteredTest.scala
@@ -0,0 +1,20 @@
+package org.elyast.orbit.system.tests.scala
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.WordSpec
+import org.scalatest.matchers.ShouldMatchers
+
+@RunWith(classOf[JUnitRunner])
+class UnfilteredTest extends WordSpec with ShouldMatchers {
+
+  "Unfiltered" should {
+    "serve web services through servlet" in {
+      
+    }
+    
+    "serve web services through netty" in {
+      
+    }
+  }
+}
